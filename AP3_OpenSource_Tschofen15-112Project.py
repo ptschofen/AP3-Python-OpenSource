c45d58958a9ebd9f9a041188b468f5318a811f15
Peter Tschofen <39842893+ptschofen@users.noreply.github.com>
2020-11-30 20:11:25 +0900

    update emission entry function

--- AP3_OpenSource_Tschofen15-112Project.py
+++ AP3_OpenSource_Tschofen15-112Project.py
@@ -1,80 +1,99 @@
 import numpy as np
+import tkinter
+
 #import pandas as pd
 #import scipy.io as sio
 #from os.path import dirname, join as pjoin
 
 path='C:/Users/ptsch/Box/AP3_Open_Access_Model/Course_Project/Model_Components'
 
-# read in emissions
-areaSources = np.genfromtxt(path + '/Emissions/area_sources_2008.csv', 
-delimiter=',')
-lowStacks = np.genfromtxt(path + '/Emissions/low_2008.csv', 
-delimiter=',')
-medStacks = np.genfromtxt(path + '/Emissions/medium_2008.csv', 
-delimiter=',')
-tallStacks = np.genfromtxt(path + '/Emissions/tall_2008.csv', 
-delimiter=',')
-tall2Stacks = np.genfromtxt(path + '/Emissions/tall2_2008.csv', 
-delimiter=',')
+def emissionVitalsEntry():
+    global year
+    global areaSources
+    global lowStackEmissions
+    global medStackEmissions
+    global tallStackEmissions
+    global tall2StackEmissions
+    year = input("Enter valid NEI year (2008, 2011, 2014 or 2017): ")
 
-# read in demographic data (population and mortality)
-population = np.genfromtxt(path + '/Population_Mortality/pop_2008.csv', 
-delimiter=',')
-mortality = np.genfromtxt(path + '/Population_Mortality/mort_2008.csv', 
-delimiter=',')
+    if not ((year == '2008') | (year == '2011') 
+            | (year == '2014') | (year == '2017')): 
+        print('Year input invalid, please enter among the options provided.')
+        year = input("Enter valid NEI year (2008, 2011, 2014 or 2017)")
+
+    # read in emissions
+    areaSources = np.genfromtxt(path + '/Emissions/area_sources_' + year +
+    '.csv', delimiter=',')
+    lowStackEmissions = np.genfromtxt(path + '/Emissions/low_' + year +
+    '.csv', delimiter=',')
+    medStackEmissions = np.genfromtxt(path + '/Emissions/medium_' + year +
+    '.csv', delimiter=',')
+    tallStackEmissions = np.genfromtxt(path + '/Emissions/tall_' + year +
+    '.csv', delimiter=',')
+    tall2StackEmissions = np.genfromtxt(path + '/Emissions/tall2_' + year +
+    '.csv', delimiter=',')
+
+    global population
+    global mortality
+    # read in demographic data (population and mortality)
+    population = np.genfromtxt(path + '/Population_Mortality/pop_' + year +
+    '.csv', delimiter=',')
+    mortality = np.genfromtxt(path + '/Population_Mortality/mort_' + year +
+    '.csv', delimiter=',')
+
+emissionVitalsEntry()
 
 # read in source/receptor matrices
 ## Area
-areaSoReNOx = np.genfromtxt(path + '/SR_Matrices/Area_NOx.csv', 
+areaSRNOx = np.genfromtxt(path + '/SR_Matrices/Area_NOx.csv', 
 delimiter=',')
-areaSoRePMVOC = np.genfromtxt(path + '/SR_Matrices/Area_PM_VOC.csv', 
+areaSRPMVOC = np.genfromtxt(path + '/SR_Matrices/Area_PM_VOC.csv', 
 delimiter=',')
-areaSoReSO2 = np.genfromtxt(path + '/SR_Matrices/Area_SO2.csv', 
+areaSRSO2 = np.genfromtxt(path + '/SR_Matrices/Area_SO2.csv', 
 delimiter=',')
-areaSoReNH3 = np.genfromtxt(path + '/SR_Matrices/Area_NH3.csv', 
+areaSRNH3 = np.genfromtxt(path + '/SR_Matrices/Area_NH3.csv', 
 delimiter=',')
 
 ## Low
-lowSoReNOx = np.genfromtxt(path + '/SR_Matrices/Low_NOx.csv', 
+lowSRNOx = np.genfromtxt(path + '/SR_Matrices/Low_NOx.csv', 
 delimiter=',')
-lowSoRePMVOC = np.genfromtxt(path + '/SR_Matrices/Low_PM_VOC.csv', 
+lowSRPMVOC = np.genfromtxt(path + '/SR_Matrices/Low_PM_VOC.csv', 
 delimiter=',')
-lowSoReSO2 = np.genfromtxt(path + '/SR_Matrices/Low_SO2.csv', 
+lowSRSO2 = np.genfromtxt(path + '/SR_Matrices/Low_SO2.csv', 
 delimiter=',')
-lowSoReNH3 = np.genfromtxt(path + '/SR_Matrices/Low_NH3.csv', 
+lowSRNH3 = np.genfromtxt(path + '/SR_Matrices/Low_NH3.csv', 
 delimiter=',')
 
 ## Medium
-medSoReNOx = np.genfromtxt(path + '/SR_Matrices/Med_NOx.csv', 
+medSRNOx = np.genfromtxt(path + '/SR_Matrices/Med_NOx.csv', 
 delimiter=',')
-medSoRePMVOC = np.genfromtxt(path + '/SR_Matrices/Med_PM_VOC.csv', 
+medSRPMVOC = np.genfromtxt(path + '/SR_Matrices/Med_PM_VOC.csv', 
 delimiter=',')
-medSoReSO2 = np.genfromtxt(path + '/SR_Matrices/Med_SO2.csv', 
+medSRSO2 = np.genfromtxt(path + '/SR_Matrices/Med_SO2.csv', 
 delimiter=',')
-medSoReNH3 = np.genfromtxt(path + '/SR_Matrices/Med_NH3.csv', 
+medSRNH3 = np.genfromtxt(path + '/SR_Matrices/Med_NH3.csv', 
 delimiter=',')
 
 ## Tall
-tallSoReNOx = np.genfromtxt(path + '/SR_Matrices/Tall_NOx.csv', 
+tallSRNOx = np.genfromtxt(path + '/SR_Matrices/Tall_NOx.csv', 
 delimiter=',')
-tallSoRePMVOC = np.genfromtxt(path + '/SR_Matrices/Tall_PM_VOC.csv', 
+tallSRPMVOC = np.genfromtxt(path + '/SR_Matrices/Tall_PM_VOC.csv', 
 delimiter=',')
-tallSoReSO2 = np.genfromtxt(path + '/SR_Matrices/Tall_SO2.csv', 
+tallSRSO2 = np.genfromtxt(path + '/SR_Matrices/Tall_SO2.csv', 
 delimiter=',')
-tallSoReNH3 = np.genfromtxt(path + '/SR_Matrices/Tall_NH3.csv', 
+tallSRNH3 = np.genfromtxt(path + '/SR_Matrices/Tall_NH3.csv', 
 delimiter=',')
 
 ## Tall2
-tall2SoReNOx = np.genfromtxt(path + '/SR_Matrices/Tall2_NOx.csv', 
+tall2SRNOx = np.genfromtxt(path + '/SR_Matrices/Tall2_NOx.csv', 
 delimiter=',')
-tall2SoRePMVOC = np.genfromtxt(path + '/SR_Matrices/Tall2_PM_VOC.csv', 
+tall2SRPMVOC = np.genfromtxt(path + '/SR_Matrices/Tall2_PM_VOC.csv', 
 delimiter=',')
-tall2SoReSO2 = np.genfromtxt(path + '/SR_Matrices/Tall2_SO2.csv', 
+tall2SRSO2 = np.genfromtxt(path + '/SR_Matrices/Tall2_SO2.csv', 
 delimiter=',')
-tall2SoReNH3 = np.genfromtxt(path + '/SR_Matrices/Tall2_NH3.csv', 
+tall2SRNH3 = np.genfromtxt(path + '/SR_Matrices/Tall2_NH3.csv', 
 delimiter=',')
 
 deaths = population * mortality
-print(sum(sum(deaths))
+#print(sum(sum(deaths))
 
-# below here is model code from Matlab copied over
