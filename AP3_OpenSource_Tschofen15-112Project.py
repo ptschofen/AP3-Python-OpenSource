6d4864fd56d414968eb5845975b09b920c5b0101
Peter Tschofen <39842893+ptschofen@users.noreply.github.com>
2020-12-01 10:07:13 +0900

    added functions on calibration coefficient readin

--- AP3_OpenSource_Tschofen15-112Project.py
+++ AP3_OpenSource_Tschofen15-112Project.py
@@ -1,5 +1,5 @@
 import numpy as np
-import tkinter
+import tkinter, winsound
 
 #import pandas as pd
 #import scipy.io as sio
@@ -7,6 +7,8 @@ import tkinter
 
 path='C:/Users/ptsch/Box/AP3_Open_Access_Model/Course_Project/Model_Components'
 
+# function that prompts user to type in year for reading in emissions,
+# stores year as a variable for other functions 
 def emissionVitalsEntry():
     global year
     global areaSources
@@ -41,59 +43,103 @@ def emissionVitalsEntry():
     mortality = np.genfromtxt(path + '/Population_Mortality/mort_' + year +
     '.csv', delimiter=',')
 
+# reads in model framework inputs (source-receptor matrices)
+def sourceReceptorReadin():
+    # read in source/receptor matrices
+    ## Area
+    areaSRNOx = np.genfromtxt(path + '/SR_Matrices/Area_NOx.csv', 
+    delimiter=',')
+    areaSRPMVOC = np.genfromtxt(path + '/SR_Matrices/Area_PM_VOC.csv', 
+    delimiter=',')
+    areaSRSO2 = np.genfromtxt(path + '/SR_Matrices/Area_SO2.csv', 
+    delimiter=',')
+    areaSRNH3 = np.genfromtxt(path + '/SR_Matrices/Area_NH3.csv', 
+    delimiter=',')
+
+    ## Low
+    lowSRNOx = np.genfromtxt(path + '/SR_Matrices/Low_NOx.csv', 
+    delimiter=',')
+    lowSRPMVOC = np.genfromtxt(path + '/SR_Matrices/Low_PM_VOC.csv', 
+    delimiter=',')
+    lowSRSO2 = np.genfromtxt(path + '/SR_Matrices/Low_SO2.csv', 
+    delimiter=',')
+    lowSRNH3 = np.genfromtxt(path + '/SR_Matrices/Low_NH3.csv', 
+    delimiter=',')
+
+    ## Medium
+    medSRNOx = np.genfromtxt(path + '/SR_Matrices/Med_NOx.csv', 
+    delimiter=',')
+    medSRPMVOC = np.genfromtxt(path + '/SR_Matrices/Med_PM_VOC.csv', 
+    delimiter=',')
+    medSRSO2 = np.genfromtxt(path + '/SR_Matrices/Med_SO2.csv', 
+    delimiter=',')
+    medSRNH3 = np.genfromtxt(path + '/SR_Matrices/Med_NH3.csv', 
+    delimiter=',')
+
+    ## Tall
+    tallSRNOx = np.genfromtxt(path + '/SR_Matrices/Tall_NOx.csv', 
+    delimiter=',')
+    tallSRPMVOC = np.genfromtxt(path + '/SR_Matrices/Tall_PM_VOC.csv', 
+    delimiter=',')
+    tallSRSO2 = np.genfromtxt(path + '/SR_Matrices/Tall_SO2.csv', 
+    delimiter=',')
+    tallSRNH3 = np.genfromtxt(path + '/SR_Matrices/Tall_NH3.csv', 
+    delimiter=',')
+
+    ## Tall2
+    tall2SRNOx = np.genfromtxt(path + '/SR_Matrices/Tall2_NOx.csv', 
+    delimiter=',')
+    tall2SRPMVOC = np.genfromtxt(path + '/SR_Matrices/Tall2_PM_VOC.csv', 
+    delimiter=',')
+    tall2SRSO2 = np.genfromtxt(path + '/SR_Matrices/Tall2_SO2.csv', 
+    delimiter=',')
+    tall2SRNH3 = np.genfromtxt(path + '/SR_Matrices/Tall2_NH3.csv', 
+    delimiter=',')
+
 emissionVitalsEntry()
+sourceReceptorReadin()
 
-# read in source/receptor matrices
-## Area
-areaSRNOx = np.genfromtxt(path + '/SR_Matrices/Area_NOx.csv', 
-delimiter=',')
-areaSRPMVOC = np.genfromtxt(path + '/SR_Matrices/Area_PM_VOC.csv', 
-delimiter=',')
-areaSRSO2 = np.genfromtxt(path + '/SR_Matrices/Area_SO2.csv', 
-delimiter=',')
-areaSRNH3 = np.genfromtxt(path + '/SR_Matrices/Area_NH3.csv', 
-delimiter=',')
+# function to read in other model parameters
+# there may be options added here for different damage functions later
+def calibrationCoefficientReadin(year):
+    global vrmr = 9186210
+    global drAdult = 0.005826891
+    global drInfant = 0.006765865
+    if year == 2008:
+        global nh4Cal = .3
+        global noxCal = .52
+        global pm25Cal = .58
+        global so4Cal = 1.1
+        global vocCal = .03
+    elif year == 2011:
+        global nh4Cal = .26
+        global noxCal = .54
+        global pm25Cal = .64
+        global so4Cal = 1.4
+        global vocCal = .023
+    elif year == 2014:
+        global nh4Cal = .28
+        global noxCal = .58
+        global pm25Cal = .72
+        global so4Cal = 1.52
+        global vocCal = .02
+    else: 
+        global nh4Cal = .3
+        global noxCal = .46
+        global pm25Cal = .76
+        global so4Cal = 1.54
+        global vocCal = .04        
 
-## Low
-lowSRNOx = np.genfromtxt(path + '/SR_Matrices/Low_NOx.csv', 
-delimiter=',')
-lowSRPMVOC = np.genfromtxt(path + '/SR_Matrices/Low_PM_VOC.csv', 
-delimiter=',')
-lowSRSO2 = np.genfromtxt(path + '/SR_Matrices/Low_SO2.csv', 
-delimiter=',')
-lowSRNH3 = np.genfromtxt(path + '/SR_Matrices/Low_NH3.csv', 
-delimiter=',')
+calibrationCoefficientReadin(year)
 
-## Medium
-medSRNOx = np.genfromtxt(path + '/SR_Matrices/Med_NOx.csv', 
-delimiter=',')
-medSRPMVOC = np.genfromtxt(path + '/SR_Matrices/Med_PM_VOC.csv', 
-delimiter=',')
-medSRSO2 = np.genfromtxt(path + '/SR_Matrices/Med_SO2.csv', 
-delimiter=',')
-medSRNH3 = np.genfromtxt(path + '/SR_Matrices/Med_NH3.csv', 
-delimiter=',')
 
-## Tall
-tallSRNOx = np.genfromtxt(path + '/SR_Matrices/Tall_NOx.csv', 
-delimiter=',')
-tallSRPMVOC = np.genfromtxt(path + '/SR_Matrices/Tall_PM_VOC.csv', 
-delimiter=',')
-tallSRSO2 = np.genfromtxt(path + '/SR_Matrices/Tall_SO2.csv', 
-delimiter=',')
-tallSRNH3 = np.genfromtxt(path + '/SR_Matrices/Tall_NH3.csv', 
-delimiter=',')
+# there is a secondary calibration step to the model that
+# I'm skipping for now
 
-## Tall2
-tall2SRNOx = np.genfromtxt(path + '/SR_Matrices/Tall2_NOx.csv', 
-delimiter=',')
-tall2SRPMVOC = np.genfromtxt(path + '/SR_Matrices/Tall2_PM_VOC.csv', 
-delimiter=',')
-tall2SRSO2 = np.genfromtxt(path + '/SR_Matrices/Tall2_SO2.csv', 
-delimiter=',')
-tall2SRNH3 = np.genfromtxt(path + '/SR_Matrices/Tall2_NH3.csv', 
-delimiter=',')
+#### input secondary calibration here ###
+
+
 
 deaths = population * mortality
-#print(sum(sum(deaths))
 
+winsound.Beep(800, 200)
