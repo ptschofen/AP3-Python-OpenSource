c9d2bef86627c898da2a7d7ef9617a09eb9ca88d
Peter Tschofen <39842893+ptschofen@users.noreply.github.com>
2020-12-04 12:28:33 +0900

    model now computes baseline concentrations correctly

--- AP3_OpenSource_Tschofen15-112Project.py
+++ AP3_OpenSource_Tschofen15-112Project.py
@@ -1,5 +1,5 @@
 import numpy as np
-import tkinter, winsound
+import tkinter, winsound, time, multiprocessing
 
 #import pandas as pd
 #import scipy.io as sio
@@ -141,26 +141,26 @@ def calibrationCoefficientReadin(year):
     global so4Cal
     global vocCal
 
-    if year == 2008:
+    if year == '2008':
         nh4Cal = .3
         noxCal = .52
         pm25Cal = .58
         so4Cal = 1.1
         vocCal = .03
-    elif year == 2011:
+    elif year == '2011':
         nh4Cal = .26
         noxCal = .54
         pm25Cal = .64
         so4Cal = 1.4
         vocCal = .023
-    elif year == 2014:
+    elif year == '2014':
         nh4Cal = .28
         noxCal = .58
         pm25Cal = .72
         so4Cal = 1.52
         vocCal = .02
     else: 
-        nh4Cal = .3
+        nh4Cal = .24
         noxCal = .46
         pm25Cal = .76
         so4Cal = 1.54
@@ -176,48 +176,59 @@ calibrationCoefficientReadin(year)
 deaths = sum(sum(population * mortality))
 
 def baselineRun():
-
+    tic = time.perf_counter()
     # Annual emission to ug/m^3 conversion
-    global NH3conv
-    global NOxconv
-    global SO2conv
-    global PMVOCconv
+    global nh3Conv
+    global noxConv
+    global pmvocConv
+    global so2Conv
 
-    NH3conv = 1.06 * 28761.72
-    NOxconv = 1.35 * 28761.72
-    SO2conv = 1.5 * 28761.72
-    PMVOCconv = 28761.72
+    nh3Conv = 1.06 * 28761.72
+    noxConv = 1.35 * 28761.72
+    so2Conv = 1.5 * 28761.72
+    pmvocConv = 28761.72
 
-    NH3 = NH3conv * (np.dot(areaSourceEmissions[:, 0],  areaSRNH3) + 
-                    np.dot(lowStackEmissions[:, 0],  lowSRNH3) + 
-                    np.dot(medStackEmissions[:, 0],  medSRNH3) + 
-                    np.dot(tallStackEmissions[:, 0],  tallSRNH3) + 
-                    np.dot(tall2StackEmissions[:, 0],  tall2SRNH3))
+    global NH3
+    global NOx
+    global PM25p
+    global SO2
+    global VOC
+    NH3 = nh3Conv * nh4Cal * (np.dot(areaSourceEmissions[:, 0], areaSRNH3) + 
+                              np.dot(lowStackEmissions[:, 0], lowSRNH3) + 
+                              np.dot(medStackEmissions[:, 0], medSRNH3) + 
+                              np.dot(tallStackEmissions[:, 0], tallSRNH3) + 
+                              np.dot(tall2StackEmissions[:, 0],tall2SRNH3))
 
-    NOx = NOxconv * (np.dot(areaSourceEmissions[:, 1],  areaSRNOx) + 
-                    np.dot(lowStackEmissions[:, 1],  lowSRNOx) + 
-                    np.dot(medStackEmissions[:, 1],  medSRNOx) + 
-                    np.dot(tallStackEmissions[:, 1],  tallSRNOx) + 
-                    np.dot(tall2StackEmissions[:, 1],  tall2SRNOx))
+    NOx = noxConv * noxCal * (np.dot(areaSourceEmissions[:, 1], areaSRNOx) + 
+                              np.dot(lowStackEmissions[:, 1], lowSRNOx) + 
+                              np.dot(medStackEmissions[:, 1], medSRNOx) + 
+                              np.dot(tallStackEmissions[:, 1], tallSRNOx) + 
+                              np.dot(tall2StackEmissions[:, 1],tall2SRNOx))
 
-    PM25p = PMVOCconv * (np.dot(areaSourceEmissions[:, 3],  areaSRPMVOC) + 
-                        np.dot(lowStackEmissions[:, 3],  lowSRPMVOC) + 
-                        np.dot(medStackEmissions[:, 3],  medSRPMVOC) + 
-                        np.dot(tallStackEmissions[:, 3],  tallSRPMVOC) + 
-                        np.dot(tall2StackEmissions[:, 3],  tall2SRPMVOC))
+    PM25p=pmvocConv*pm25Cal * (np.dot(areaSourceEmissions[:, 3], areaSRPMVOC) + 
+                               np.dot(lowStackEmissions[:, 3], lowSRPMVOC) + 
+                               np.dot(medStackEmissions[:, 3], medSRPMVOC) + 
+                               np.dot(tallStackEmissions[:, 3], tallSRPMVOC) + 
+                               np.dot(tall2StackEmissions[:, 3], tall2SRPMVOC))
 
-    SO2 = SO2conv * (np.dot(areaSourceEmissions[:, 4],  areaSRSO2) + 
-                    np.dot(lowStackEmissions[:, 4],  lowSRSO2) + 
-                    np.dot(medStackEmissions[:, 4],  medSRSO2) + 
-                    np.dot(tallStackEmissions[:, 4],  tallSRSO2) + 
-                    np.dot(tall2StackEmissions[:, 4],  tall2SRSO2))
+    SO2 = so2Conv * so4Cal * (np.dot(areaSourceEmissions[:, 4], areaSRSO2) + 
+                              np.dot(lowStackEmissions[:, 4], lowSRSO2) + 
+                              np.dot(medStackEmissions[:, 4], medSRSO2) + 
+                              np.dot(tallStackEmissions[:, 4], tallSRSO2) + 
+                              np.dot(tall2StackEmissions[:, 4], tall2SRSO2))
 
-    VOC = PMVOCconv * (np.dot(areaSourceEmissions[:, 5],  areaSRPMVOC) + 
-                    np.dot(areaSourceEmissions[:, 6],  areaSRPMVOC) + 
-                    np.dot(lowStackEmissions[:, 5],  lowSRPMVOC) + 
-                    np.dot(medStackEmissions[:, 5],  medSRPMVOC) + 
-                    np.dot(tallStackEmissions[:, 5],  tallSRPMVOC) + 
-                    np.dot(tall2StackEmissions[:, 5],  tall2SRPMVOC))
+    VOC = pmvocConv * vocCal * (np.dot(areaSourceEmissions[:, 5], areaSRPMVOC) + 
+                                np.dot(areaSourceEmissions[:, 6], areaSRPMVOC) + 
+                                np.dot(lowStackEmissions[:, 5], lowSRPMVOC) + 
+                                np.dot(medStackEmissions[:, 5], medSRPMVOC) + 
+                                np.dot(tallStackEmissions[:, 5], tallSRPMVOC) + 
+                                np.dot(tall2StackEmissions[:, 5],tall2SRPMVOC))
+
+    global NH4
+    global NH4e
+    global HNO3
+    global NO3
+    global SO4
 
     NH4 = NH3
     NH4e = (NH3 / 18 - 1.5 * SO2 / 96)
@@ -231,49 +242,14 @@ def baselineRun():
     # compute baseline PM25 concentrations
     global PM25b
     PM25b = NO3 + SO4 + VOC + PM25p + NH4
+    toc = time.perf_counter()
+    print(f"Performed baseline run in {toc - tic:0.4f} seconds")
 
 baselineRun()
 print(PM25b)
 ### Matlab Code for what comes next
 
 '''
-
-    run Nitrate_Sulfate_Ammonium
-
-%% Compute concentrations of particulate nitrate, ammonium, and sulfate.
-% 18 = MW particulate NH4
-% 96 = MW particulate Sulfate
-% 0.12028 = (1/8.314)PPM to Mole
-% 339.9329 = (101300/298) PPM to Mole
-% 0.01595 = 1/MW PNO3 = 62
-% 0.00039 = (273K+10C)*(Gaseous Nitric Acid in ug/m^3)/(1,000*(12.187*62))
-%   This yields gaseous nitric acid in ppm then coverted to moles as above.
-
-%% Predictions from AP3
-
-NH4(:,1) = (NH3(:,1) + NH3(:,2) + NH3(:,3) + NH3(:,4) + NH3(:,5));
-SO4(:,1) = (SO2(:,1) + SO2(:,2) + SO2(:,3) + SO2(:,4) + SO2(:,5));
-NH4e(:,1) = (NH4(:,1)./18 - 1.5.*SO4(:,1)./96);
-HNO3(:,1) = (NOx(:,1) + NOx(:,2) + NOx(:,3) + NOx(:,4) + NOx(:,5))./62;
-
-HNO3_Base = HNO3;
-
-% Specifications from Charles: Fit 1
-NO3(:,1) = 0.6509.*(0.33873.*HNO3+0.121008.*NH4e+ 3.511482.*(HNO3.*NH4e)).*62;
-
-% NO3(:,1) = 0.385.*(exp(0.008267.*LN_HNO3(:,1)) +  (0.178005.*NH4e(:,1))./0.01595);
-SO4(:,1) = (1.375.*(SO2(:,1) + SO2(:,2)+ SO2(:,3)+ SO2(:,4) + SO2(:,5)));
-
-% Assemble species into total PM_25
-PM_25(:,1) = (NO3(:,1)+SO4(:,1)+A_VOC(:,1)+A_VOC(:,2)+A_VOC(:,3)+A_VOC(:,4) + 
-A_VOC(:,5) + B_VOC(:,1)+PM_25_Primary(:,1) +  PM_25_Primary(:,2) + PM_25_Primary(:,3) + 
-PM_25_Primary(:,4)+ PM_25_Primary(:,5) + NH4);
-
-    run PM_25_Base_Raw
-PM_25_B(:,1) = (NO3(:,1)+SO4(:,1)+A_VOC(:,1)+A_VOC(:,2)+A_VOC(:,3)+A_VOC(:,4) + 
-A_VOC(:,5) +B_VOC(:, 1) + PM_25_Primary(:,1) +  PM_25_Primary(:,2) + PM_25_Primary(:,3) + 
-PM_25_Primary(:,4)+ PM_25_Primary(:,5)+NH4);
-
     run PM_25_Health_Base
 
 % Pope, 2002 = 0.005826891 (BENMAP)
